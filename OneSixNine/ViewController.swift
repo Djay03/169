527e41d7b3a2cd617967254ed1d0606349a14f41
Ray <ray@rayps.com>
2017-01-04 00:37:24 +0800

    CameraEngine

--- OneSixNine/ViewController.swift
+++ OneSixNine/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var shootButton: UIButton!
+    
+    let cameraEngine = CameraEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cameraEngine.startSession()
+        shootButton.clipsToBounds = true
+        shootButton.layer.cornerRadius = 50
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        if let layer = self.cameraEngine.previewLayer {
+            layer.frame = cameraView.bounds
+            cameraView.layer.insertSublayer(layer, at: 0)
+            cameraView.layer.masksToBounds = true
+        }
+        
     }
-
-
+    
+    @IBAction func shootButtonTapped(_ sender: Any) {
+        self.cameraEngine.capturePhoto { (image , error) -> (Void) in
+            if let image = image {
+                CameraEngineFileManager.savePhoto(image, blockCompletion: { (success, error) -> (Void) in
+                    if success {
+                        let alertController =  UIAlertController(title: "Success, image saved !", message: nil, preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
 }
 
